2232699dd36bb2ca0a2e12a720c396e31b88967c
TaylorAndTony <doyijian@163.com>
2022-11-05 11:55:40 +0800

    add special chars

--- make_mess.py
+++ make_mess.py
@@ -32,10 +32,10 @@ class Gui:
         self.root = Tk()
         self.root.title('GBK from UTF-8 messy code')
         self.font = ('微软雅黑', 12)
-        
+
         self.input_text = StringVar()
         self.output_text = StringVar()
-        
+
         self.input_text_test = StringVar()
         self.output_text_test = StringVar()
 
@@ -44,28 +44,55 @@ class Gui:
         style.configure('.', font=self.font)
 
     def init_ui(self):
+        # create messy code
         f1 = Frame(self.root)
         f1.pack(**GAP)
         Label(f1, text="Make messy code when decode utf-8 in gbk").pack()
-        input_e = Entry(f1, width=60, font=self.font ,textvariable=self.input_text)
-        input_e.pack(**GAP)
-        input_e.focus_set()
-        input_e.bind('<KeyRelease>', self.on_input)
-        Entry(f1, width=60, font=self.font ,textvariable=self.output_text).pack(**GAP)
-        
+        self.input_e = Entry(f1,
+                             width=60,
+                             font=self.font,
+                             textvariable=self.input_text)
+        self.input_e.pack(**GAP)
+        self.input_e.focus_set()
+        self.input_e.bind('<KeyRelease>', self.on_input)
+        # insert special characters
+        f11 = Frame(f1)
+        f11.pack()
+        codes = ['…', '□', '◇', '○', '⋌', '∷']
+        # pack all special buttons
+        for idx in range(len(codes)):
+            b = Button(f11,
+                       width=2,
+                       text=codes[idx],
+                       command=lambda idx=idx: self.handle_btn(codes[idx]))
+            b.pack(side=LEFT, **GAP)
+        Entry(f1,
+              width=60,
+              font=self.font,
+              textvariable=self.output_text).pack(**GAP)
+
+        # decode messy code
         f2 = Frame(self.root)
         f2.pack(**GAP)
         Label(f2, text="Reverse test").pack()
-        input_test = Entry(f2, width=60, font=self.font ,textvariable=self.input_text_test)
+        input_test = Entry(f2,
+                           width=60,
+                           font=self.font,
+                           textvariable=self.input_text_test)
         input_test.pack(**GAP)
         input_test.bind('<KeyRelease>', self.on_input_test)
-        Entry(f2, width=60, font=self.font ,textvariable=self.output_text_test).pack(**GAP)
+        Entry(f2, width=60, font=self.font,
+              textvariable=self.output_text_test).pack(**GAP)
         return self
-    
+
+    def handle_btn(self, char):
+        self.input_e.insert(INSERT, char)
+        self.on_input()
+
     def on_input(self, *a, **b):
         text = self.input_text.get()
         self.output_text.set(make_mess(text))
-    
+
     def on_input_test(self, *a, **b):
         text = self.input_text_test.get()
         self.output_text_test.set(make_mess(text, True))
